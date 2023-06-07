3e5e3cf24c40c93487e180db5283b3d97ddcd142
Evgeny Konnov <evgen.nn.ru@rambler.ru>
2023-06-07 22:45:04 +0300

    add ButtonClick

--- app/src/main/java/com/example/myapplicationb/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationb/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.myapplicationb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+
+        // Получаем ссылку на кнопку
+        val registerButton = findViewById<Button>(R.id.login_activity_button)
+
+        // Устанавливаем слушатель нажатий
+        registerButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
