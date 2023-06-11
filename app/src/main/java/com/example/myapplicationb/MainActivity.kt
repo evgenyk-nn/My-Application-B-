2c14067290bb7ad102d0ef8c8947c70ce5c4d384
Evgeny Konnov <evgen.nn.ru@rambler.ru>
2023-06-12 00:48:39 +0300

    add plugins email, fb, phone,twitter, google

--- app/src/main/java/com/example/myapplicationb/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationb/MainActivity.kt
@@ -6,60 +6,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var database: DatabaseReference
-
-    // array list
-    private val signInLauncher = registerForActivityResult(
-        //создали объект авторизации
-        FirebaseAuthUIActivityResultContract(),
-    ) { resultCallback ->
-        this.onSignInResult(resultCallback) // запуск экрана
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        database = Firebase.database.reference
-        database.child("users").child("userId").setValue("user")
-    }
 
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult?) {
-        //
+
+        // Получаем ссылку на кнопку
+        val registerButton = findViewById<Button>(R.id.login_activity_button)
+
+        // Устанавливаем слушатель нажатий
+        registerButton.setOnClickListener {
+            val intentToAnotherScreen = Intent(this, RegistrationActivity::class.java)
+            startActivity(intentToAnotherScreen)
+
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
+        // Получаем ссылку на текстовое поле
+        val registerTextView = findViewById<TextView>(R.id.login_activity_text)
+
+        // Устанавливаем слушатель нажатий
+        registerTextView.setOnClickListener {
+            val intentToAnotherScreen = Intent(this, LoginActivity::class.java)
+            startActivity(intentToAnotherScreen)
+
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
-
-
-
-
-
-
-
-//        // Получаем ссылку на кнопку
-//        val registerButton = findViewById<Button>(R.id.login_activity_button)
-//
-//        // Устанавливаем слушатель нажатий
-//        registerButton.setOnClickListener {
-//            val intentToAnotherScreen = Intent(this, RegistrationActivity::class.java)
-//            startActivity(intentToAnotherScreen)
-//
-//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        // Получаем ссылку на текстовое поле
-//        val registerTextView = findViewById<TextView>(R.id.login_activity_text)
-//
-//        // Устанавливаем слушатель нажатий
-//        registerTextView.setOnClickListener {
-//            val intentToAnotherScreen = Intent(this, LoginActivity::class.java)
-//            startActivity(intentToAnotherScreen)
-//
-//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-//        }
---
+++ app/src/main/java/com/example/myapplicationb/activity_login.java
@@ -0,0 +1,6 @@
+package com.example.myapplicationb;
+
+import android.app.Activity;
+
+public class activity_login extends Activity {
+}
