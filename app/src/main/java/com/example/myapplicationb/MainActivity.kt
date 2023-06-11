3d3038d73d37d3eaf2fc9103f30b05fdfc0a9d8e
Evgeny Konnov <evgen.nn.ru@rambler.ru>
2023-06-11 10:09:02 +0300

    add bookworm_logo

--- app/src/main/java/com/example/myapplicationb/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationb/MainActivity.kt
@@ -7,10 +7,12 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         // Получаем ссылку на кнопку
         val registerButton = findViewById<Button>(R.id.login_activity_button)
