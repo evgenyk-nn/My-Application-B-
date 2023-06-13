81b938af209455df17f4c11b79d49367aa82e327
Evgeny Konnov <evgen.nn.ru@rambler.ru>
2023-06-13 22:45:26 +0300

    changing main and registration

--- app/src/main/java/com/example/myapplicationb/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationb/MainActivity.kt
@@ -3,55 +3,91 @@ package com.example.myapplicationb
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(    // создание объекта авторизации
+        FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        this.onSignInResult(res)        // запуск самого экрана
+    }
+
+    private lateinit var database: DatabaseReference         // Второй этап (создание объекта для записи в БД)
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Получаем ссылку на кнопку
-        val registerButton = findViewById<Button>(R.id.login_activity_button)
+        // Choose authentication providers
+        Log.d("testLogs", "RegistrationActivity start registration")
+        database = Firebase.database.reference          // инициализация базы данных
 
-        // Устанавливаем слушатель нажатий
-        registerButton.setOnClickListener {
-            val intentToAnotherScreen = Intent(this, RegistrationActivity::class.java)
-            startActivity(intentToAnotherScreen)
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())     // список плагинов регистрации
 
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-        }
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()                                        // создали интента для экрана firebase
+        signInLauncher.launch(signInIntent)                 // запуск экрана firebase auth
+    }
 
-        // Получаем ссылку на текстовое поле
-        val registerTextView = findViewById<TextView>(R.id.login_activity_text)
 
-        // Устанавливаем слушатель нажатий
-        registerTextView.setOnClickListener {
-            val intentToAnotherScreen = Intent(this, LoginActivity::class.java)
-            startActivity(intentToAnotherScreen)
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse        // результат с экрана firebase auth
+        if (result.resultCode == RESULT_OK) {                // проверка результата на OK
+            Log.d("testLogs", "RegistrationActivity registration success ${response?.email}")
 
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            // Successfully signed in
+            val authUser = FirebaseAuth.getInstance().currentUser   // создание объекта текущего пользователя firebase auth
+            authUser?.let {             // если объект существует - сохраняем его в БД
+                val email = it.email.toString()         // извлекаем email пользователя
+                val uid = it.uid                        //  извлекаем uid
+                val firebaseUser = User(email, uid)            // создаем новый объект user  с параметрами email и uid
+                Log.d("testLogs","RegistrationActivity firebaseUser $firebaseUser")
+                database.child("users").child(uid).setValue(firebaseUser)       // сохраняем пользователя в Firebase Realtime
+                onBackPressed()
+            }
+
+            // ...
+        } else {                  // обработка ошибки, если результат не OK
+            Log.d("testLogs", "RegistrationActivity registration failure")
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
         }
     }
+
+
 }
 
 
 
 
 
-// ###########################################################################################
 
 
-// books and uid
+
 
 //package com.example.myapplicationb
-
+//
 //import android.content.Intent
 //import androidx.appcompat.app.AppCompatActivity
 //import android.os.Bundle
@@ -65,25 +101,32 @@ class MainActivity : AppCompatActivity() {
 //import com.google.firebase.ktx.Firebase
 //
 //class MainActivity : AppCompatActivity() {
-//    private lateinit var database: DatabaseReference
-//
-//    //array list
-//    private val signUpLauncher = registerForActivityResult( //создали объект авторизации
-//        FirebaseAuthUIActivityResultContract(),
-//    ) { resultCallback ->
-//
-//        this.onSignInResult(resultCallback) // запуск самого экрана
-//    }
-//
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-//        database = Firebase.database.reference
-//        database.child("users").child("userId").setValue("user")
-//        // ...
-//    }
 //
-//    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult?) {
-//        //
+//        // Получаем ссылку на кнопку
+//        val registerButton = findViewById<Button>(R.id.login_activity_button)
+//
+//        // Устанавливаем слушатель нажатий
+//        registerButton.setOnClickListener {
+//            val intentToAnotherScreen = Intent(this, RegistrationActivity::class.java)
+//            startActivity(intentToAnotherScreen)
+//
+//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        // Получаем ссылку на текстовое поле
+//        val registerTextView = findViewById<TextView>(R.id.login_activity_text)
+//
+//        // Устанавливаем слушатель нажатий
+//        registerTextView.setOnClickListener {
+//            val intentToAnotherScreen = Intent(this, LoginActivity::class.java)
+//            startActivity(intentToAnotherScreen)
+//
+//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//        }
 //    }
 //}
+
+
--- app/src/main/java/com/example/myapplicationb/RegistrationActivity.kt
+++ app/src/main/java/com/example/myapplicationb/RegistrationActivity.kt
@@ -55,8 +55,8 @@ class RegistrationActivity : AppCompatActivity() {
                 val firebaseUser = User(email, uid)            // создаем новый объект user  с параметрами email и uid
                 Log.d("testLogs","RegistrationActivity firebaseUser $firebaseUser")
                 database.child("users").child(uid).setValue(firebaseUser)       // сохраняем пользователя в Firebase Realtime
+                onBackPressed()
             }
-
 
         // ...
         } else {                  // обработка ошибки, если результат не OK
@@ -68,6 +68,96 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.example.myapplicationb
+//
+//import android.os.Bundle
+//import android.util.Log
+//import androidx.appcompat.app.AppCompatActivity
+//import com.firebase.ui.auth.AuthUI
+//import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+//import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.database.DatabaseReference
+//import com.google.firebase.database.ktx.database
+//import com.google.firebase.ktx.Firebase
+//
+//class RegistrationActivity : AppCompatActivity() {
+//
+//    private val signInLauncher = registerForActivityResult(    // создание объекта авторизации
+//        FirebaseAuthUIActivityResultContract(),
+//    ) { res ->
+//        this.onSignInResult(res)        // запуск самого экрана
+//    }
+//
+//
+//    private lateinit var database: DatabaseReference         // Второй этап (создание объекта для записи в БД)
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_registration)
+//
+//        // Choose authentication providers
+//        Log.d("testLogs", "RegistrationActivity start registration")
+//        database = Firebase.database.reference          // инициализация базы данных
+//
+//        val providers = arrayListOf(
+//            AuthUI.IdpConfig.EmailBuilder().build())     // список плагинов регистрации
+//
+//        // Create and launch sign-in intent
+//        val signInIntent = AuthUI.getInstance()
+//            .createSignInIntentBuilder()
+//            .setAvailableProviders(providers)
+//            .build()                                        // создали интента для экрана firebase
+//        signInLauncher.launch(signInIntent)                 // запуск экрана firebase auth
+//    }
+//
+//    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+//        val response = result.idpResponse        // результат с экрана firebase auth
+//        if (result.resultCode == RESULT_OK) {                // проверка результата на OK
+//            Log.d("testLogs", "RegistrationActivity registration success ${response?.email}")
+//
+//            // Successfully signed in
+//            val authUser = FirebaseAuth.getInstance().currentUser   // создание объекта текущего пользователя firebase auth
+//            authUser?.let {             // если объект существует - сохраняем его в БД
+//                val email = it.email.toString()         // извлекаем email пользователя
+//                val uid = it.uid                        //  извлекаем uid
+//                val firebaseUser = User(email, uid)            // создаем новый объект user  с параметрами email и uid
+//                Log.d("testLogs","RegistrationActivity firebaseUser $firebaseUser")
+//                database.child("users").child(uid).setValue(firebaseUser)       // сохраняем пользователя в Firebase Realtime
+//                onBackPressed()
+//            }
+//
+//            // ...
+//        } else {                  // обработка ошибки, если результат не OK
+//            Log.d("testLogs", "RegistrationActivity registration failure")
+//            // Sign in failed. If response is null the user canceled the
+//            // sign-in flow using the back button. Otherwise check
+//            // response.getError().getErrorCode() and handle the error.
+//            // ...
+//        }
+//    }
+//}
 
 
 
